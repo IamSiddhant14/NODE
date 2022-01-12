bd5d6e587b010d11959fe4acbed11d005ff88946
Siddhant <siddhantsharma669@gmail.com>
2022-01-13 01:08:01 +0530

    Modularity

---
+++ NODE/Modularity/moduleTest.js
@@ -0,0 +1,6 @@
+const calculatorModule = require('./calculator');
+
+calculatorModule.division(20,5);
+calculatorModule.addition(20,5);
+calculatorModule.Multiply(20,5);
+calculatorModule.substract(20,5);
