55b71e10abf0fa3daa8ad002470ce271920fb4c0
Siddhant <siddhantsharma669@gmail.com>
2022-01-23 13:08:38 +0530

    Added organizer function to the fs file

--- NODE/File system orginiser/Fo.js
+++ NODE/File system orginiser/Fo.js
@@ -16,10 +16,17 @@
 
 // Argv stands for argument vector
 
+///////////////////////////////////////////////
+
+// ALLWAYS OPEN THE INTEGRATED TERMINAL IN FO.JS FOLDER OR ONLY OR IT WOULD RESULT IN AN ERROR
+
+//////////////////////////////////////////////
+
+const fs = require('fs')
+const path = require('path')
+
 let inputArr = process.argv.slice(2);
 // let input = process.argv[2]
-
-
 
 
 //This will only give us the second index of the character that is -- Siddhant
@@ -36,10 +43,10 @@ switch(command){
     case 'tree':
         console.log('Tree implemented')
         break;
-    case 'oranganize':
-        console.log('Organize implented')
+    case 'organize'://node Fo.js organize '<--Filepath of folder which we wish to organize-->'
+        organizeFn(inputArr[1])
         break;
-    case 'help':
+    case 'help'://node Fo.js help
         helpfn()
         break;
     default:
@@ -53,6 +60,35 @@ function helpfn(){
                1) Tree command - node FO.js tree <dirname>
                2) Organize Command - node FO.js oraganize <dirname>
                3) Help Command - node FO.js help `)
+}
+
+// This function will create an folder named organized files in which we would be having sevrals files like that of media , document , app , archives where the files would be sorted in a proper manner
+
+ 
+function organizeFn(dirpath){//input of a directory path
+
+    let destPath ;
+
+    if(dirpath == undefined){
+        console.log('Please Enter a Directory Path')
+        return;
+    }else{
+        let doesExist = fs.existsSync(dirpath)
+        console.log(doesExist)
+
+        if(doesExist == true){
+            destPath = path.join(dirpath ,"organized_files")
+
+            if( fs.existsSync(destPath) == false ){
+                fs.mkdirSync(destPath)
+            }else{
+                console.log('This folder already exist')
+            }
+
+        }else{
+            console.log('Please entre a valid path')
+        }
+    }
 }
 
 
